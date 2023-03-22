b64b1008a73b80eb211c87137767077209a0a918
Kapee87 <kapserker@gmail.com>
2023-03-22 16:37:39 -0300

    webConfig first deploy

--- src/main/java/com/portfolioNamontanerAp/namontaner/controller/PersonaController.java
+++ src/main/java/com/portfolioNamontanerAp/namontaner/controller/PersonaController.java
@@ -5,7 +5,6 @@ import com.portfolioNamontanerAp.namontaner.entity.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 public class PersonaController {
 
     @Autowired
